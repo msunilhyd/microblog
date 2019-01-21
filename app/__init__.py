09ba46ae12e6b893e0253068bea2d4a1c64ad7fd
msunilhyd <msunilhyd@gmail.com>
2019-01-21 07:02:47 -0800

    21-Jan-19 : Email support for password reset

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from logging.handlers import RotatingFileHandler
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
